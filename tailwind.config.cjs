29078cfc62d60cb8f7221b9569f285a44c5fff99
Nauzet Hernández <nauzet.aduen@gmail.com>
2023-10-10 02:51:36 +0100

    Improve buttons with hover effect

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,11 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			fontFamily:{ 'custom': ["Atkinson"]}
+			fontFamily:{ 'custom': ["Atkinson"]},
+			backgroundSize: {
+				visible3: "100% 15px",
+				invisible0: "0% 15px"
+			  },
 		},
 	},
 	plugins: [],
