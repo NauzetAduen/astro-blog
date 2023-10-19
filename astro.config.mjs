4ea3ceff36b2d9479f37adcc6f8bb172d2abb3d0
Nauzet Hernández <nauzet.aduen@gmail.com>
2023-10-19 01:43:16 +0100

    Add base head to each page

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   server: { port: 4321, host: true },
-  site: "https://example.com",
+  site: "https://nauzet.vercel.app",
   integrations: [mdx(), sitemap(), tailwind()],
   experimental: {
     viewTransitions: true,
