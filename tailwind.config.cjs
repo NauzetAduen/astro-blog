ad5dd5b84f52d106e850a8444f7fcdaf805d2761
NauzetAduen <nauzet.aduen@gmail.com>
2024-08-17 03:30:49 +0100

    Refactor colors in tailwind config, Add new colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,27 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily:{ 'custom': ["Atkinson"]},
-			backgroundSize: {
-				visibleSmall: "100% 15px",
-				visibleBig: "100% 100%",
-				invisibleSmall: "0% 15px",
-				invisibleBig: "0% 100%"
-			  },
-			  colors: {
-				richBlack: "#0D1F2D",
-				payneGrey: "#546a7b",
-				whiteSmoke: "#F6F4F4",
-				springGreen: "#37FF8B",
-				fireEngineRed: "#D62828"
-
-
-			  }
-		},
-	},
-	plugins: [
-		require('@tailwindcss/typography')
-	],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: { custom: ["Atkinson"] },
+      backgroundSize: {
+        visibleSmall: "100% 15px",
+        visibleBig: "100% 100%",
+        invisibleSmall: "0% 15px",
+        invisibleBig: "0% 100%",
+      },
+      colors: {
+        textColor: "#0D1F2D",
+        greyColor: "#A9A6A0",
+        backgroundColor: "#F8F1EA",
+        primaryAccent: "#2AFC98",
+        secondaryAccent: "#C1292E",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
